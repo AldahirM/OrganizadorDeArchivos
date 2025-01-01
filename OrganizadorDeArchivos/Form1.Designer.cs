b6995a86a8718168e90971e5c0599542609421f5
AldahirM <l21te0275@teziutlan.tecnm.mx>
2025-01-01 15:55:15 -0600

    first commit

---
+++ OrganizadorDeArchivos/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace OrganizadorDeArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Attrib = new Button();
+            btnOrdenar = new Button();
+            listBox1 = new ListBox();
+            btnSeleccionar = new Button();
+            listBox2 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // Attrib
+            // 
+            Attrib.BackColor = Color.Snow;
+            Attrib.Font = new Font("Showcard Gothic", 9.75F);
+            Attrib.Location = new Point(12, 178);
+            Attrib.Name = "Attrib";
+            Attrib.Size = new Size(150, 42);
+            Attrib.TabIndex = 8;
+            Attrib.Text = "Ejecutar Attrib";
+            Attrib.UseVisualStyleBackColor = false;
+            Attrib.Click += Attrib_Click;
+            // 
+            // btnOrdenar
+            // 
+            btnOrdenar.BackColor = Color.Snow;
+            btnOrdenar.Font = new Font("Showcard Gothic", 9.75F);
+            btnOrdenar.Location = new Point(12, 103);
+            btnOrdenar.Name = "btnOrdenar";
+            btnOrdenar.Size = new Size(150, 43);
+            btnOrdenar.TabIndex = 7;
+            btnOrdenar.Text = "Ordenar";
+            btnOrdenar.UseVisualStyleBackColor = false;
+            btnOrdenar.Click += btnOrdenar_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.MintCream;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(168, 37);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(387, 334);
+            listBox1.TabIndex = 6;
+            // 
+            // btnSeleccionar
+            // 
+            btnSeleccionar.BackColor = Color.Snow;
+            btnSeleccionar.Font = new Font("Showcard Gothic", 9.75F);
+            btnSeleccionar.Location = new Point(12, 35);
+            btnSeleccionar.Name = "btnSeleccionar";
+            btnSeleccionar.Size = new Size(150, 44);
+            btnSeleccionar.TabIndex = 5;
+            btnSeleccionar.Text = "Seleccionar Ruta";
+            btnSeleccionar.UseVisualStyleBackColor = false;
+            btnSeleccionar.Click += btnSeleccionar_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = Color.MintCream;
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(168, 416);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(387, 109);
+            listBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(210, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(299, 27);
+            label1.TabIndex = 10;
+            label1.Text = "ARCHIVOS ENCONTRADOS";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(161, 386);
+            label2.Name = "label2";
+            label2.Size = new Size(406, 27);
+            label2.TabIndex = 11;
+            label2.Text = "ARCHIVOS CAMBIADOS DE NOMBRE";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(573, 554);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox2);
+            Controls.Add(Attrib);
+            Controls.Add(btnOrdenar);
+            Controls.Add(listBox1);
+            Controls.Add(btnSeleccionar);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Attrib;
+        private Button btnOrdenar;
+        private ListBox listBox1;
+        private Button btnSeleccionar;
+        private ListBox listBox2;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ OrganizadorDeArchivos/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+
+namespace OrganizadorDeArchivos
+{
+    public partial class Form1 : Form
+    {
+        private string rutaOrigen = "";
+        private List<string> archivos;
+        string comando = "";
+        Thread t;
+        const string pattern = @"\bdesktop\.ini\b";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ActualizarListBox()
+        {
+            listBox1.Items.Clear();
+            if (archivos != null && archivos.Count > 0)
+            {
+                foreach (string archivo in archivos)
+                {
+                    listBox1.Items.Add(Path.GetFileName(archivo));
+                }
+            }
+        }
+        private void LlamarComando()
+        {
+            ExecuteCommand(comando);
+        }
+        public void ExecuteCommand(string comand)
+        {
+            // Crear un proceso de la l�nea de comandos
+            Process cmdProcess = new Process();
+
+            // Configurar las propiedades del proceso
+            cmdProcess.StartInfo.FileName = "cmd.exe"; // El ejecutable de la l�nea de comandos
+            cmdProcess.StartInfo.RedirectStandardInput = true; // Redirigir la entrada est�ndar
+            cmdProcess.StartInfo.RedirectStandardOutput = true; // Redirigir la salida est�ndar
+            cmdProcess.StartInfo.UseShellExecute = false; // No utilizar el shell de Windows
+            cmdProcess.StartInfo.CreateNoWindow = true; // No crear una ventana para el proceso
+
+            // Iniciar el proceso
+            cmdProcess.Start();
+
+            // Obtener el flujo de entrada est�ndar del proceso
+            var standardInput = cmdProcess.StandardInput;
+
+            // Ejecutar comandos en la l�nea de comandos
+            standardInput.WriteLine(comand);
+
+            // Cerrar la entrada est�ndar para indicar que no se enviar�n m�s comandos
+            standardInput.Close();
+
+            // Leer la salida est�ndar del proceso
+            string output = cmdProcess.StandardOutput.ReadToEnd();
+
+            // Esperar a que el proceso termine
+            cmdProcess.WaitForExit();
+
+            // Mostrar la salida en la consola de C#
+
+            this.comando = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                rutaOrigen = dialog.SelectedPath;
+
+                // Actualiza el ListBox con los nombres de los archivos
+                archivos = new List<string>(Directory.GetFiles(rutaOrigen));
+                ActualizarListBox();
+            }
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            string extension = "";
+            string carpetaDestino = "";
+            string nomArchivo = "";
+            string rutaDestino = "";
+            if (archivos == null || archivos.Count == 0)
+            {
+                MessageBox.Show("Por favor, seleccione una carpeta antes de ordenar los archivos.",
+                    "Carpeta no seleccionada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                foreach (string archivo in archivos)
+                {
+
+                    if (Regex.IsMatch(archivo, pattern))
+                    {
+                        return;
+                    }
+                    extension = Path.GetExtension(archivo);
+                    if (extension.ToLower() == ".docx" || extension.ToLower() == ".xlsx" || extension.ToLower() == ".pptx" || extension.ToLower() == ".doc")
+                    {
+                        carpetaDestino = Path.Combine(rutaOrigen, "Office");
+                        carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                        checkDirectoryExist(carpetaDestino);
+                        return;
+
+                    }
+                    if (extension.ToLower() == ".jpg" || extension.ToLower() == ".jpeg" || extension.ToLower() == ".png" || extension.ToLower() == ".gif" || extension.ToLower() == ".jfif" || extension.ToLower() == ".webp" || extension.ToLower() == ".wbmp" || extension.ToLower() == ".raw" || extension.ToLower() == ".psd"
+                            || extension.ToLower() == ".tiff" || extension.ToLower() == ".eps")
+                    {
+                        carpetaDestino = Path.Combine(rutaOrigen, "Imagenes");
+                        carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                        checkDirectoryExist(carpetaDestino);
+                        return;
+                    }
+
+                    if (extension.ToLower() == ".mp4" || extension.ToLower() == ".avi" || extension.ToLower() == ".mkv" || extension.ToLower() == ".mov" || extension.ToLower() == ".wmv" || extension.ToLower() == ".flv" || extension.ToLower() == ".webm" || extension.ToLower() == ".mpg" || extension.ToLower() == ".3gp")
+                    {
+                        carpetaDestino = Path.Combine(rutaOrigen, "Videos");
+                        carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                        checkDirectoryExist(carpetaDestino);
+                        return;
+                    }
+
+                    if (extension.ToLower() == ".zip" || extension.ToLower() == ".rar" || extension.ToLower() == ".7z" || extension.ToLower() == ".tar" || extension.ToLower() == ".gz" || extension.ToLower() == ".bz2" || extension.ToLower() == ".xz" || extension.ToLower() == ".cab" || extension.ToLower() == ".z"
+                            || extension.ToLower() == ".iso")
+                    {
+                        carpetaDestino = Path.Combine(rutaOrigen, "Comprimido");
+                        carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                        checkDirectoryExist(carpetaDestino);
+                        return;
+                    }
+
+                    if (extension.ToLower() == ".exe" || extension.ToLower() == ".app" || extension.ToLower() == ".dmg" || extension.ToLower() == ".bin" || extension.ToLower() == ".bat" || extension.ToLower() == ".sh" || extension.ToLower() == ".jar" || extension.ToLower() == ".apk" || extension.ToLower() == ".msi"
+                            || extension.ToLower() == ".cmd")
+                    {
+                        carpetaDestino = Path.Combine(rutaOrigen, "Ejecutables");
+                        carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                        checkDirectoryExist(carpetaDestino);
+                        return;
+                    }
+
+                    if (extension.ToLower() == ".mp3" || extension.ToLower() == ".wav" || extension.ToLower() == ".flac" || extension.ToLower() == ".aac" || extension.ToLower() == ".ogg" || extension.ToLower() == ".wma" || extension.ToLower() == ".aiff" || extension.ToLower() == ".m4a" || extension.ToLower() == ".mid"
+                                            || extension.ToLower() == ".opus")
+                    {
+                        carpetaDestino = Path.Combine(rutaOrigen, "Música");
+                        carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                        checkDirectoryExist(carpetaDestino);
+                        return;
+                    }
+
+                    carpetaDestino = Path.Combine(rutaOrigen, "Otros");
+                    carpetaDestino = Path.Combine(carpetaDestino, extension.TrimStart('.'));
+                    checkDirectoryExist(carpetaDestino);                        
+
+                    nomArchivo = Path.GetFileName(archivo);
+                    rutaDestino = Path.Combine(carpetaDestino, nomArchivo);
+                    checkFileExist(rutaDestino, nomArchivo, carpetaDestino);
+
+                        /*if (File.Exists(rutaDestino))
+                        {
+                            nombreArchivo2 += aux2 + " -> ";
+                            while (File.Exists(rutaDestino))
+                            {
+                                auxn = auxn.Replace(".", "1.");
+                                aux2 = aux2.Replace(".", "1.");
+                                rutaDestino = Path.Combine(carpetaDestino, aux2);
+                                File.Move(archivoOrg, auxn);
+                                archivoOrg = archivoOrg.Replace(".", "1.");
+
+                            }
+                            auxn = rutaOrigen + "\\" + aux2;
+                            File.Move(auxn, rutaDestino);
+                            nombreArchivo2 += Path.GetFileName(auxn);
+                            listBox2.Items.Add(nombreArchivo2);
+                            nombreArchivo2 = "";
+                        }
+                        else
+                        {
+                            File.Move(archivo, rutaDestino);
+                        }*/
+                    
+                }
+
+                MessageBox.Show("Los archivos se han ordenado exitosamente.", "Ordenamiento completado",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ActualizarListBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurri� un error al intentar ordenar los archivos: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            ActualizarListBox();
+        }
+
+        private void checkFileExist(string path, string nomArchivo, string carpetaDestino)
+        {
+            if (File.Exists(path))
+            {
+                while (File.Exists(path))
+                {
+                    File.Move()
+                }
+                auxn = rutaOrigen + "\\" + aux2;
+                File.Move(auxn, rutaDestino);
+                nombreArchivo2 += Path.GetFileName(auxn);
+                listBox2.Items.Add(nomArchivo + " -> " + Path.GetFileName(auxn));
+                nombreArchivo2 = "";
+            }
+        }
+
+        private void checkDirectoryExist(string path)
+        {
+            if (!(Directory.Exists(path)))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private void Attrib_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((rutaOrigen.Length > 0))
+                {
+                    comando = rutaOrigen[0] + rutaOrigen[1] + "\n" + "cd " + "\"" + rutaOrigen + "\"" + "\n" + "attrib -s -h -r /s /d *.*" + "\n";
+                    if (comando.Contains("126"))
+                    {
+                        comando = comando.Replace("126", "");
+                    }
+                    t = new Thread(LlamarComando);
+                    t.Start();
+                    MessageBox.Show("COMANDO ATTRIB EJECUTANDOSE", "INFORMACI�N", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Selecciona una ruta primero");
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("HUBO UN ERROR XD");
+            }
+        }
+    }
+}
